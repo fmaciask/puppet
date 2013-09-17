4708ebc602224adb3559be2e6853d94437d039c6
Forlidar Macias <forlidar@forlidar.com>
2013-09-17 11:45:27 +0000

    Completing the module config for Mysql

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,5 +1,22 @@
 class mysql {
-	ensure => true,
-	hasstatus => true,
-notify {"Modulo mysql":}
+#	ensure => true,
+#	hasstatus => true,
+#notify {"Modulo mysql":}
+	package { 'mysql-server' :
+		ensure => installed,
+	}
+	file { '/etc/mysql/my.cnf' : 
+		source => 'puppet:///modules/mysql/files',
+		owner => 'root',
+		group => 'root',
+		mode => '640',
+		require => Package['mysql-server'],
+	}
+	service { 'sql':
+		ensure => running,
+		enable => true,
+		hasstatus => true,
+		hasrestart => true,
+	}
 }
+include mysql
