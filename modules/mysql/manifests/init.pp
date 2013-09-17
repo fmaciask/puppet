5d855c1fde929874a54dfaed7fb0f30b20df1d55
Forlidar Macias <forlidar@forlidar.com>
2013-09-17 14:04:34 +0000

    Unsetting the file requirement

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -5,13 +5,13 @@ class mysql {
 	package { 'mysql-server' :
 		ensure => installed,
 	}
-	file { '/etc/mysql/my.cnf' : 
-		source => '/etc/puppet/modules/mysql/files',
-		owner => 'root',
-		group => 'root',
-		mode => '640',
-		require => Package['mysql-server'],
-	}
+	#file { '/etc/mysql/my.cnf' : 
+	#	source => '/etc/puppet/modules/mysql/files',
+	#	owner => 'root',
+	#	group => 'root',
+	#	mode => '640',
+	#	require => Package['mysql-server'],
+	#}
 	service { 'mysql':
 		ensure => running,
 		enable => true,
