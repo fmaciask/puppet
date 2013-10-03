504d524dc7f9e54bfe7642238f6207bd9b9912f2
Forlidar Macias <forlidar@forlidar.com>
2013-10-03 15:32:39 +0000

    Stable for installation

--- modules/hiawatha/manifests/install.pp
+++ modules/hiawatha/manifests/install.pp
@@ -3,8 +3,8 @@ class hiawatha::install {
 	include cmake
 	exec { "dpkg -i /etc/puppet/modules/hiawatha/files/hiawatha_9.2_amd64.deb":
 		onlyif => [ "whereis hiawatha | cut -d: -f2" ],
-		path =>	["/usr/bin","/usr/sbin"],
-
+		#path =>	["/usr/bin","/usr/sbin"],
+		path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 	}
 	# Requiere cmake para realizar la instalación
 	Class['cmake'] ~> Class['hiawatha::install']
