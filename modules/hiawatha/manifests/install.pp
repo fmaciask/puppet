b7aeb06ce2ea9d4b6b26b6a7fcdffd3ad07c5c4c
Forlidar Macias <forlidar@forlidar.com>
2013-10-03 16:01:44 +0000

    Clean and well-commented

--- modules/hiawatha/manifests/install.pp
+++ modules/hiawatha/manifests/install.pp
@@ -1,13 +1,10 @@
 class hiawatha::install {
 	
-	include cmake
+	#In the next step, the complete file MUST be inside the right path
+	# I download the files from http://www.hiawatha-webserver.org/download
 	exec { "dpkg -i /etc/puppet/modules/hiawatha/files/hiawatha_9.2_amd64.deb":
-		#onlyif => [ "whereis hiawatha | cut -d: -f2" ],
-		#subscribe =>  File["/usr/sbin/hiawatha"],
-		#refreshonly => true,
+		
 		path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 		creates => "/usr/sbin/hiawatha",
 	}
-	# Requiere cmake para realizar la instalación
-	Class['cmake'] ~> Class['hiawatha::install']
 }
