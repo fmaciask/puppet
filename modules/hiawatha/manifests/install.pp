efd07fb97fb1aba1955291bb809bada4ca7e62a9
Forlidar Macias <forlidar@forlidar.com>
2013-10-03 15:26:42 +0000

    fixing error in line

--- modules/hiawatha/manifests/install.pp
+++ modules/hiawatha/manifests/install.pp
@@ -3,6 +3,8 @@ class hiawatha::install {
 	include cmake
 	exec { "dpkg -i /etc/puppet/modules/hiawatha/files/hiawatha_9.2_amd64.deb":
 		onlyif => [ "whereis hiawatha | cut -d: -f2" ],
+		path =>	["/usr/bin","/usr/sbin"],
+
 	}
 	# Requiere cmake para realizar la instalación
 	Class['cmake'] ~> Class['hiawatha::install']
