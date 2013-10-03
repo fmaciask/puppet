6b2fb4f22b106bb23e2cd2d455fb44b5ec89433f
Forlidar Macias <forlidar@forlidar.com>
2013-10-03 14:04:33 +0000

    Install file update

--- modules/hiawatha/manifests/install.pp
+++ modules/hiawatha/manifests/install.pp
@@ -2,5 +2,8 @@ class hiawatha::install {
 	#package { "hiawatha":
 	#	ensure => "installed",
 	#}
-	exec {  }
+	exec { "dpkg -i /etc/puppet/modules/hiawatha/files/hiawatha_9.2_i686.deb":
+		onlyif => [ "whereis hiawatha | cut -d: -f2" ],
+		#whereis hiawatha | cut -d: -f2
+	}
 }
