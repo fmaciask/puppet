b293e15c1aa7bfd9f84f08cf25d254bc229040ba
Forlidar Macias <forlidar@forlidar.com>
2013-10-03 15:39:05 +0000

    Trying to avoid reinstall

--- modules/hiawatha/manifests/install.pp
+++ modules/hiawatha/manifests/install.pp
@@ -2,8 +2,9 @@ class hiawatha::install {
 	
 	include cmake
 	exec { "dpkg -i /etc/puppet/modules/hiawatha/files/hiawatha_9.2_amd64.deb":
-		onlyif => [ "whereis hiawatha | cut -d: -f2" ],
-		#path =>	["/usr/bin","/usr/sbin"],
+		#onlyif => [ "whereis hiawatha | cut -d: -f2" ],
+		subscribe =>  File["/usr/sbin/hiawatha"],
+		refreshonly => true,
 		path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 	}
 	# Requiere cmake para realizar la instalación
