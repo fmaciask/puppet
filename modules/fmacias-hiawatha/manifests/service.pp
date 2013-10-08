c4372bc179baec6311e2a660f203122d2c3be520
Forlidar Macias <forlidar@forlidar.com>
2013-10-08 08:27:58 +0000

    Improved way to start the webserver, for any distro is the same

--- modules/fmacias-hiawatha/manifests/service.pp
+++ modules/fmacias-hiawatha/manifests/service.pp
@@ -5,22 +5,21 @@
 # This manifest manages service status.
 #
 class hiawatha::service {
-#	service { 'hiawatha':
-#		ensure => running,
-#		enable => true,
-#		hasstatus => true,
-#		status => 0,
-#		start => 'service hiawatha start',
-#		stop => 'service hiawatha stop',
+	
+# This is the standar way to start a service on Ubuntu (Debian distro)
+#	if $lsbdistid == 'Ubuntu' {
+#		exec { 'execute hiawatha service':
+#				command => "/usr/bin/service hiawatha start",
+#		}
 #	}
-	if $lsbdistid == 'Ubuntu' {
-		exec { 'execute hiawatha service':
-				command => "/usr/bin/service hiawatha start",
-		}
+#	else {
+#		exec { "/etc/init.d/hiawatha start": }
+#	} 
+#	
+# hiawatha webserver can be run just executing the name of the service
+	exec { "hiawatha":
+		path => $path,
 	}
-	else {
-		exec { "/etc/init.d/hiawatha start": }
-	} 
 }
 
 include hiawatha::service
