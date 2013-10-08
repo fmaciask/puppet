050c99e4a0399ef556d8c3c7d9bdf522e9b86d61
Forlidar Macias <forlidar@forlidar.com>
2013-10-08 09:02:13 +0000

    Changed the name of the folder

--- modules/fmacias-hiawatha/manifests/config.pp
+++
@@ -1,9 +0,0 @@
-# Name of manifest: config.pp
-# This manifests helps configuring parameters for Hiawatha webserver
-class hiawatha::config {
-	file { '${hiawatha::params::$hwt_confdir}/hiawatha.conf':
-		ensure => file,
-		require => Package['hiawatha'],
-		content => template('templates/hiawatha.conf.erb'),
-	}
-}
