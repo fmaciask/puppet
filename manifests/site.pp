81766ce5c5ea5d6674b9f1e43b46b9d24bf9c348
Forlidar Macias <forlidar@forlidar.com>
2013-11-11 12:38:00 +0000

    Adding aaaaall of the modules currently  configured.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,8 @@
 # /etc/puppet/manifest/site.pp
-#include nginx
+include nginx
 include mysql
-#include gcc
-#include cmake
-#include hiawatha
-#include smartmontools
+include gcc
+include cmake
+include hiawatha
+include smartmontools
 include iftop
