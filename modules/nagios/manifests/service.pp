ee17f2720e44748bdb829f4801d6495ab298d4c2
Forlidar Macias <forlidar@forlidar.com>
2013-11-25 14:11:58 +0000

    Update service configuration for Nagios module

--- modules/nagios/manifests/service.pp
+++ modules/nagios/manifests/service.pp
@@ -2,15 +2,15 @@ class nagios::service {
 	package { "nagios3":
 		ensure => installed,
 		enable => true,
-		notify => Service['nagios3'],
+		notify => Service['nagios3']
 	}
 	package { "nagios-nrpe-plugin":
 		ensure => installed,
 		enable => true,
 	}
-	service {"nagios3":
+	service { 'nagios3':
 		ensure => running,
-		require => Package['nagios3'],
+		require => Package['nagios3']
 	}
 	
 }
