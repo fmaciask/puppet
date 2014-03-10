2f5a279d39b4b4a56629d918e8d40ac1da2c2afa
Forlidar Macias <forlidar@forlidar.com>
2014-03-10 15:34:13 +0000

    VLC new installation

---
+++ modules/vlc/manifests/init.pp
@@ -0,0 +1,14 @@
+# Public: Install VLC.app to /Applications.
+#
+# Examples
+#
+#   include vlc
+class vlc (
+  $version = '2.0.3'
+) {
+  package { 'VLC':
+#    provider => 'appdmg',
+#    source   => "http://sourceforge.net/projects/vlc/files/${version}/macosx/vlc-${version}.dmg",
+	ensure => installed,
+  }
+}
